dbb125320d2f5e2e1ca9ddf011faa9ba8691d05c
htmlcodeee-eng <htmlcodeee@gmail.com>
2025-11-07 09:22:01 +0530

    Create jobs.js

---
+++ css/js/jobs.js
@@ -0,0 +1,27 @@
+const jobListings = [
+  {
+    "id": 1,
+    "title": "Software Development Engineer",
+    "company": "Amazon",
+    "location": "Bengaluru, India",
+    "description": "Seeking a talented SDE to build scalable software solutions. Must have a strong background in data structures and algorithms.",
+    "applyLink": "https://www.amazon.jobs/en/"
+  },
+  {
+    "id": 2,
+    "title": "Marketing Manager",
+    "company": "Google",
+    "location": "Remote",
+    "description": "Join our dynamic marketing team to lead and execute innovative campaigns for Google's latest products.",
+    "applyLink": "https://careers.google.com/"
+  },
+  {
+    "id": 3,
+    "title": "Data Analyst",
+    "company": "Netflix",
+    "location": "Mumbai, India",
+    "description": "We are looking for a data-driven individual to analyze trends and provide insights that will shape our content strategy.",
+    "applyLink": "https://jobs.netflix.com/"
+  }
+  // Add your new job listings here!
+];
